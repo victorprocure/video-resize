016881e1d009063c63764e14114f730954c897a5
Victor Procure <victor.procure@gmail.com>
2023-02-11 11:54:03 -0700

    feat: Added proper logging

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,5 @@
 using CommandLine;
+using Microsoft.Extensions.Logging;
 using VideoResize.Configuration;
 
 namespace VideoResize;
@@ -7,19 +8,33 @@ public class Program
 {
     public static async Task<int> Main(params string[] args)
     {
+        var logger = CreateLogger();
+
         return await Parser.Default.ParseArguments<CommandLineOptions>(args)
             .MapResult(async (CommandLineOptions opts) =>
             {
                 try
                 {
-                    Console.WriteLine(opts.Path);
+                    logger.LogInformation("Application started for Path: {0}, using Recursive: {1}", opts.Path, opts.Recursive);
                     return await Task.FromResult(1);
                 }
-                catch
+                catch(Exception ex)
                 {
+                    logger.LogError(ex, "Unexpected exception!");
                     return -3;
                 }
             },
             errs => Task.FromResult(-1));
+    }
+
+    private static ILogger CreateLogger()
+    {
+        using var loggerFactory = LoggerFactory.Create(b =>
+                {
+                    b
+                    .AddConsole();
+                });
+
+        return loggerFactory.CreateLogger<Program>();
     }
 }
