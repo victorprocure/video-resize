1ab64d75db498feb293bd7ae15756005092718db
Victor Procure <victor.procure@gmail.com>
2023-02-11 11:59:25 -0700

    fix: Fix configuration options

--- src/Configuration/CommandLineOptions.cs
+++ src/Configuration/CommandLineOptions.cs
@@ -7,6 +7,6 @@ public class CommandLineOptions
     [Value(index: 0, Required = true, HelpText = "Path to videos.")]
     public string Path { get; set; } = default!;
 
-    [Option(shortName: 'r', longName: "recursive", Required = false, HelpText = "Recursively search videos", Default = true)]
+    [Option(shortName: 'r', Required = false, HelpText = "Recursively search videos", Default = true)]
     public bool Recursive { get; set; }
 }
